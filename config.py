752f21a0896f0b60baa682b7456e94bfa48a83ec
zxvaibhav <opmodsforyou@gmail.com>
2025-09-25 10:08:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://filestoredrobot.onrender.com")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
